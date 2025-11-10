8236daebef13cb0feab07c4b4f9e367dddfba465
Felipe Ceacero <fe_ceacero@id.uff.br>
2025-11-09 23:57:40 -0300

    Subindo script com as análises iniciais

---
+++ script1.R
@@ -0,0 +1,79 @@
+
+setwd('C:\\Users\\ssser\\Downloads')
+
+library(readxl)
+dados <- read_excel("Base_trabalho.xlsx")
+
+
+str(dados) #Verificando estrutura da base e quais variáveis serão transformadas em
+           #fator.
+dados <- dados |>
+  mutate(across(c(sexo, filhos, escolaridade, casado), as.factor))
+
+sum(is.na(dados)) 
+
+library(naniar)
+miss_var_summary(dados) #Aparentemente nenhum dado faltante
+
+library(ggplot2)#Pacote para construção dos gráficos
+#Histograma da variável Idade.
+ggplot(dados, aes(x = idade)) +
+  geom_histogram(color = "black", fill = "lightblue", bins = 20) +
+  labs(
+    title = "Histograma da variável Idade",
+    x = "Idade",
+    y = "Frequência"
+  ) +
+  theme_minimal()+
+  theme(
+    plot.title = element_text(hjust = 0.5)
+  )
+
+#Boxplot da variável tempo_preso
+ggplot(dados, aes(x = "", y = tempo_preso)) +
+  geom_boxplot(fill = "lightblue", color = "black") +
+  labs(
+    title = "Boxplot da variável Tempo Preso",
+    x = "",
+    y = "Tempo Preso"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5)
+  )
+
+
+#Agora o boxplot da variável score_periculosidade por escolaridade
+ggplot(dados, aes(x = escolaridade, y = score_periculosidade)) +
+  geom_boxplot(fill = "lightblue") +
+  scale_x_discrete(
+    labels = c("1" = "Fundamental", 
+               "2" = "Médio", 
+               "3" = "Superior")
+  ) +
+  labs(
+    title = "Score de Periculosidade por Escolaridade",
+    x = "Escolaridade",
+    y = "Score de Periculosidade"
+  ) +
+  theme_minimal()+
+  theme(
+    plot.title = element_text(hjust = 0.5)
+  )
+
+#Gráfico de barras para a variável reincidente
+ggplot(dados, aes(x = reincidente)) +
+  geom_bar(fill = "lightblue", color = "black") +
+  labs(
+    title = "Frequência de Reincidência",
+    x = "Reincidente",
+    y = "Contagem"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = 0.5)
+  )
+
+
+
+
